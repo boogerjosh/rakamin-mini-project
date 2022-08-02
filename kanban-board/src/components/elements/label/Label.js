4e7f53f9f27fad3a80f4695ed7f16a04499a025f
DESKTOP-T522386\JEC <it.dev.albedo1@jec.co.id>
2022-08-02 08:38:53 +0700

    label card item color

--- kanban-board/src/components/elements/label/Label.js
+++ kanban-board/src/components/elements/label/Label.js
@@ -4,11 +4,33 @@ import './index.css';
 const Label = (props) => {
   const { title } = props;
   return (
-    <div className='label-container'>
-        <div>
-          <p>{title}</p>
+    <>
+      {title == "Group Task 2" && <div className='label-container' style={{borderColor: "#FEEABC", background: "#FFFCF5"}}>
+         <div>
+         <p style={{color: "#FA9810"}}>{title}</p>
         </div>
-    </div>
+     </div> }
+     {title == "Group Task 3" && <div className='label-container' style={{borderColor: "#F5B1B7", background: "#FFFAFA"}}>
+         <div>
+         <p style={{color: "#E11428"}}>{title}</p>
+        </div>
+     </div> }
+     {title == "Group Task 4" && <div className='label-container' style={{borderColor: "#B8DBCA", background: "#F8FBF9"}}>
+         <div>
+         <p style={{color: "#43936C"}}>{title}</p>
+        </div>
+     </div> }
+     {title == "Group Task 1" && <div className='label-container'>
+         <div>
+         <p>{title}</p>
+        </div>
+     </div> }
+    </>
+    // <div className='label-container'>
+    //     <div>
+    //       <p>{title}</p>
+    //     </div>
+    // </div>
   );
 };
 
--- kanban-board/src/components/progress/Progress.js
+++ kanban-board/src/components/progress/Progress.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Draggable } from "react-beautiful-dnd";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import './index.css'
 
 function Progress({ item, index }) {
   
@@ -15,18 +17,20 @@ function Progress({ item, index }) {
           >
               <p className='card-name'>{item.name}</p>
               <div className='line-dashed'></div>
-              <div className='progress'>
-              { item.progress_percentage == 100 ? <div className='bar-percentage'>
-                  <div style={{width: item.progress_percentage + '%', backgroundColor: "#43936C", borderRadius: '7px'}} className='percentage' data-percent='70%'></div>
-              </div> : <div className='bar-percentage'>
-                  <div style={{width: item.progress_percentage + '%'}} className='percentage' data-percent='70%'></div>
-              </div>}
-              <div className='number-percent'>{item.progress_percentage + '%'}</div>
-              <div className="horizontal-dots">
+              <div className='progress-count'>
+                <div className='progress'>
+                  { item.progress_percentage == 100 ? <div className='bar-percentage'>
+                      <div style={{width: item.progress_percentage + '%', backgroundColor: "#43936C", borderRadius: '7px'}} className='percentage' data-percent='70%'></div>
+                  </div> : <div className='bar-percentage'>
+                      <div style={{width: item.progress_percentage + '%'}} className='percentage' data-percent='70%'></div>
+                  </div>}
+                  { item.progress_percentage == 100 ? <CheckCircleIcon className='completed'/> : <div className='number-percent'>{item.progress_percentage + '%'}</div> }
+                </div>
+                <div className="horizontal-dots">
                   <span></span>
                   <span></span>
                   <span></span>
-              </div>
+                </div>
               </div>
           </div>
         )}
